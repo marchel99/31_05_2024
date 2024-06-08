41055ca1bae61b2ff086eeec2cfa995c1d7dec00
marchel99 <kubamarchelski@live.com>
2024-06-08 17:10:14 +0200

    add: new library - sen05xx.h

---
+++ Libraries/stm32-SEN05XX-master/sen05xx.h
@@ -0,0 +1,10 @@
+/******************************************************************************
+Copyright (C) 2024
+File : sen05xx.h
+Author : Marchel99
+****************************************************************************.*/
+
+
+//Te dyrektywy zapobiegają wielokrotnemu dołączeniu tego samego pliku nagłówkowego, co mogłoby prowadzić do błędów kompilacji.
+#ifndef _SEN05XX_H_
+#define _SEN05XX_H_
